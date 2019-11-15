ef984b487f2372ee0704dbb76ecd7c7f05ae4fd2
medjadjichaimaa99 <57466637+medjadjichaimaa99@users.noreply.github.com>
2019-11-15 12:01:46 +0100

    my first commit

---
+++ src/components/Comments.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class='comment'>
+    <h1>{{com}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:"Comments",
+     props: {
+    com: String
+  },
+  
+    data () 
+    {
+       return {
+           message :'texte saisi :',
+           saisie :''
+       }
+    },
+
+
+    filters: 
+    {
+        ajoute : function (valeur,saisie){
+            return valeur+saisie;
+        }
+    }
+}
+
+</script>
